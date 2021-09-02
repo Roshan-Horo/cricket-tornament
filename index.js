97e084d16e5c6f44927b3336d60e94010cf9d6e9
Roshan-Horo <roshanhoro@gmail.com>
2021-09-02 23:44:19 +0530

    test new config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app')
 const pool = require('./config/pool')
 require('dotenv').config()
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3005
 
 // const devConfig = {
 //     user: process.env.PG_USER,
@@ -12,15 +12,22 @@ const PORT = process.env.PORT || 8000
 //     port: process.env.PG_PORT
 // }
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 
-const prodConfig = process.env.DATABASE_URL
+const devConfig = {
+    connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+}
 
-pool.connect({
-    connectionString: process.env.NODE_ENV === 'production' ? prodConfig : devConfig
-}) 
+const prodConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+      }
+}
+
+pool.connect(process.env.NODE_ENV === 'production' ? prodConfig : devConfig) 
 .then( () => {
 app().listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
 })
 .catch(err => console.error(err))
+
 
